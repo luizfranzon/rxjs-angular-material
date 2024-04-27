c29bf5f0d9edf46ca8450ecab9a6f0c2100d0cb4
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-04-27 14:33:08 -0300

    create observable and observer

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
-import { from } from "rxjs"
+import { from, Observable } from "rxjs"
 
-let numbers = [1,5,10]
+let numbers = [1,2,3,4,5]
 let source = from(numbers)
 
+let sourceInstance = new Observable(subscriber => {
+  for (let number of numbers) {
+    if (number > 4) {
+      subscriber.error("Erro foda")
+    }
+
+    subscriber.next(number)
+  }
+  subscriber.complete()
+})
+
+const myObserver = {
+  next: (number: number) => console.log(number),
+  error: (error: Error) => console.error(error),
+  complete: () => console.log("Completed")
+}
+
 function component() {
-  source.subscribe({
-    next: (x) => console.log(x)
-  })
+  source.subscribe(myObserver)
+  console.log("------------------------")
+  sourceInstance.subscribe(myObserver)
 }
 
 component()
