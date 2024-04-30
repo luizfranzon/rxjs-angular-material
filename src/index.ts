9dc2e01310f8740899a9e929492adbea003d4beb
Luiz Fernando Franzon <luizfranzon05@hotmail.com>
2024-04-30 11:40:56 -0300

    first operators

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,32 @@
-import { from, Observable } from "rxjs"
+import { Observable } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
-let numbers = [1,2,3,4,5]
-let source = from(numbers)
+let numbers = [1, 2, 3, 4, 5];
 
-let sourceInstance = new Observable(subscriber => {
-  for (let number of numbers) {
-    if (number > 4) {
-      subscriber.error("Erro foda")
+let source = new Observable((subscriber) => {
+  let index = 0;
+  function produceValue() {
+    subscriber.next(numbers[index++]);
+
+    if (index < numbers.length) {
+      setTimeout(produceValue, 500);
+    } else {
+      subscriber.error("Erro esperado");
     }
 
-    subscriber.next(number)
+    // subscriber.complete();
   }
-  subscriber.complete()
-})
+  produceValue();
+});
 
-const myObserver = {
-  next: (number: number) => console.log(number),
+source.pipe(map((n: number) => n * 2)).subscribe({
+  next: (number: number) => console.log(`X - Result: ${number}`),
   error: (error: Error) => console.error(error),
-  complete: () => console.log("Completed")
-}
+  complete: () => console.log("Completed"),
+});
 
-function component() {
-  source.subscribe(myObserver)
-  console.log("------------------------")
-  sourceInstance.subscribe(myObserver)
-}
-
-component()
+source.pipe(filter((n: number) => n > 2)).subscribe({
+  next: (number: number) => console.log(`Y - Result: ${number}`),
+  error: (error: Error) => console.error(error),
+  complete: () => console.log("Completed"),
+});
